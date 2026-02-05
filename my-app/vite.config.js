a7c9a4237f4896c72049f2483fddfbd326e08f60
takhdeer <tgrew803@mtroyal.ca>
2026-02-04 17:01:33 -0700

    front end email API integration

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
+    port: 3000,
+    open: true
+  }
 })
